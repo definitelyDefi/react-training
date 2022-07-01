e5565cf655cf028bedee2a896076750c84b2e514
Denis Beliy <yuichirohakya227@gmail.com>
2022-07-01 16:33:11 +0300

    Updates

---
+++ precache-manifest.6efb8d31f71a9d3cc22b781f857d80a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "591095ec515cba5071a2",
+    "url": "/react-training/static/css/main.c974442b.chunk.css"
+  },
+  {
+    "revision": "591095ec515cba5071a2",
+    "url": "/react-training/static/js/main.591095ec.chunk.js"
+  },
+  {
+    "revision": "83cc8ce480b90eb34697",
+    "url": "/react-training/static/js/1.83cc8ce4.chunk.js"
+  },
+  {
+    "revision": "e28cad9f477cf2211002",
+    "url": "/react-training/static/js/runtime~main.e28cad9f.js"
+  },
+  {
+    "revision": "67b5ea4345a18fa40e71aa46e1e98e60",
+    "url": "/react-training/index.html"
+  }
+];
