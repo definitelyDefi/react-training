ebc7a374df0f88dc56f7cd230ddc4fab14c2f89d
Denis Beliy <yuichirohakya227@gmail.com>
2022-07-23 18:44:43 +0300

    Updates

---
+++ precache-manifest.d594bd350ea5722287afffe18954fcf7.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f8a7057e88806f8383f",
+    "url": "/react-training/static/css/main.07e39df0.chunk.css"
+  },
+  {
+    "revision": "8f8a7057e88806f8383f",
+    "url": "/react-training/static/js/main.8f8a7057.chunk.js"
+  },
+  {
+    "revision": "6bbadf14ae4317ae1f49",
+    "url": "/react-training/static/js/reactPlayerYouTube.6bbadf14.chunk.js"
+  },
+  {
+    "revision": "656dd9010c6b77efd37c",
+    "url": "/react-training/static/js/reactPlayerSoundCloud.656dd901.chunk.js"
+  },
+  {
+    "revision": "0066875c6db6bde3a3e3",
+    "url": "/react-training/static/js/reactPlayerVimeo.0066875c.chunk.js"
+  },
+  {
+    "revision": "6565cc9060833523e19b",
+    "url": "/react-training/static/js/reactPlayerFacebook.6565cc90.chunk.js"
+  },
+  {
+    "revision": "f628f97cadf5b2ff44b0",
+    "url": "/react-training/static/js/reactPlayerStreamable.f628f97c.chunk.js"
+  },
+  {
+    "revision": "92628f9f5982efce8721",
+    "url": "/react-training/static/js/reactPlayerWistia.92628f9f.chunk.js"
+  },
+  {
+    "revision": "4761d7a7c8134a1947d0",
+    "url": "/react-training/static/js/reactPlayerTwitch.4761d7a7.chunk.js"
+  },
+  {
+    "revision": "1cb81f17762c8714533c",
+    "url": "/react-training/static/js/reactPlayerDailyMotion.1cb81f17.chunk.js"
+  },
+  {
+    "revision": "292a48418de49dab3a20",
+    "url": "/react-training/static/js/reactPlayerMixcloud.292a4841.chunk.js"
+  },
+  {
+    "revision": "79969cac867f42d391ae",
+    "url": "/react-training/static/js/reactPlayerVidyard.79969cac.chunk.js"
+  },
+  {
+    "revision": "333ca6ba8152e5024ab5",
+    "url": "/react-training/static/js/reactPlayerKaltura.333ca6ba.chunk.js"
+  },
+  {
+    "revision": "f105f49c3c601aa97fdf",
+    "url": "/react-training/static/js/reactPlayerFilePlayer.f105f49c.chunk.js"
+  },
+  {
+    "revision": "fb8a448db562676d4b22",
+    "url": "/react-training/static/js/reactPlayerPreview.fb8a448d.chunk.js"
+  },
+  {
+    "revision": "90c530a7dfa5f92ba8b6",
+    "url": "/react-training/static/css/14.629c3f56.chunk.css"
+  },
+  {
+    "revision": "90c530a7dfa5f92ba8b6",
+    "url": "/react-training/static/js/14.90c530a7.chunk.js"
+  },
+  {
+    "revision": "56ae5fbc7f915a8027cd",
+    "url": "/react-training/static/js/runtime~main.56ae5fbc.js"
+  },
+  {
+    "revision": "9aef2836c5fa378ea824d531b3734eb0",
+    "url": "/react-training/static/media/search.9aef2836.svg"
+  },
+  {
+    "revision": "b4b249733992c6e843ae0b9eca9a46cc",
+    "url": "/react-training/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-training/precache-manifest.efd2252689d5650306595a98e47156d6.js"
+  "/react-training/precache-manifest.d594bd350ea5722287afffe18954fcf7.js"
 );
 
 workbox.clientsClaim();
